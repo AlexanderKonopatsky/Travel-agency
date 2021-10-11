9d974fa7ea4d588c98a792784caa5ca0f9465c28
Alexander <alex.conopatscky@yandex.ru>
2021-10-11 17:17:43 +0300

    form-react

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Footer from "../Footer";
+import Form from "../Form";
 
 function Home() {
   return (
     <>
-      <h1 className="home">Home</h1>
+      <h1 className="home"></h1>
+      <Form />
       <Footer />
     </>
   )
