d1331afb13683831af18ba54442421934a975670
Alexander <alex.conopatscky@yandex.ru>
2021-10-12 00:19:08 +0300

    update card

--- frontend/src/components/CardItem.js
+++ frontend/src/components/CardItem.js
@@ -6,7 +6,16 @@ function CardItem(props) {
   return (
     <>
       <li className='cards__item'>
+
+
         <Link className='cards__item__link' to={props.path}>
+
+          <div class='tour-card__favorite'>
+            <button aria-label='Add to favorites' class='favorite-toggle' data-add-favorite-url='/account/favorites' data-favorite-toggle >
+              <i class="far fa-heart"></i>
+            </button>
+          </div>
+
           <figure className='cards__item__pic-wrap' data-category={props.label}>
             <img
               className='cards__item__img'
@@ -14,9 +23,53 @@ function CardItem(props) {
               src={props.src}
             />
           </figure>
-          <div className='cards__item__info'>
-            <h5 className='cards__item__text'>{props.text}</h5>
+          <div className="cards_cont">
+            {/* <div className='cards__item__info'> */}
+            <h3 className='cards__item__text'>{props.text}</h3>
+            {/*      </div> */}
+            <p className="tour_card_description">{props.desc}</p>
+
+            <div class='rating'>
+              <span class='star-rating'>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <i class='fa fa-star'></i>
+                <span class='star-rating__count'>
+                  (173)
+                </span>
+              </span>
+            </div>
           </div>
+
+          <div>
+            <div class='tour-card__details'>
+              <div class='tour-detail'>
+                <div class='tour-detail__text'>
+                  $305
+                </div>
+                <span class='label'>
+                  Price Tour
+                </span>
+              </div>
+              <div class='tour-detail'>
+                <div class='tour-detail__text'>
+                  Dop info
+                </div>
+                <span class='label'>
+
+                </span>
+              </div>
+              {/*<div class='tour-card__cta action-button'>
+                Book now
+              </div> */}
+            </div>
+
+          </div>
+
+
+
         </Link>
       </li>
     </>
--- frontend/src/components/Cards.js
+++ frontend/src/components/Cards.js
@@ -12,15 +12,17 @@ function Cards() {
             <ul className='cards__items'>
               <CardItem
                 src='images/img-1.jpg'
-                text='The Last Supper Painting Tour: Da Vinci in Depth. '
+                text='The Last'
                 label='label'
                 path='/'
+                desc='Luomo: Private Boat Excursion'
               />
               <CardItem
                 src='images/img-2.jpg'
                 text='Luxury Lake Como: Private Boat Excursion'
                 label='label'
                 path='/'
+                desc='Alta M In MilanAlta Moda: Fashion In MilanAlta Moda: Fashion In MilanAlta Moda: Fashion In MilanAlta Moda: Fashion In MilanAlta Moda: Fashion In Milan'
               />
             </ul>
             <ul className='cards__items'>
@@ -29,24 +31,28 @@ function Cards() {
                 text='Alta Moda: Fashion In Milan'
                 label='label'
                 path='/'
+                desc='The Last Supper Pain Denci in DepthThe Last Supper Painting Tour: Da Vinci in Dep: Da Vinci in Depth'
               />
               <CardItem
                 src='images/img-4.jpg'
                 text='The Last Supper Painting Tour: Da Vinci in Depth'
                 label='label'
                 path='/'
+                desc='Ride through the Sahara Desert on a guided camel tourRide through the Sahara Ded camel tour'
               />
               <CardItem
                 src='images/img-5.jpg'
                 text='Ride through the Sahara Desert on a guided camel tour'
                 label='label'
                 path='/'
+                desc='Ride through the Saharahara esert on a guided camel tour'
               />
               <CardItem
                 src='images/img-6.jpg'
                 text='Luxury Lake Como: Private Boat Excursion'
                 label='label'
                 path='/'
+                desc='Ride through the Sahara Desert on amel tourRide through the Sahara Desert on a guided camel tour'
               />
             </ul>
           </div>
